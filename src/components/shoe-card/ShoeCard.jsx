8ef331a6d1aa3ae9a92e8157990f3bc8bffa3f16
Frederick M. Rogers <fmrogers@carbonium.no>
2020-05-29 18:19:41 +0200

    Fixed a problem with the path generetaed by 'shoeCard' was inncorrect, by extracting the correct portion of the path from 'routerProps' and assigning it manually

--- src/components/shoe-card/ShoeCard.jsx
+++ src/components/shoe-card/ShoeCard.jsx
@@ -5,28 +5,32 @@ import { FaStar } from "react-icons/fa";
 import styles from "./ShoeCard.module.scss";
 
 export const ShoeCard = (props) => {
-	const { name, images, category, price, productid, featured } = props.product;
-	const membershipCost = Math.ceil(price - (price / 100) * 5);
-	return (
-		<div className={styles.shoeCard} data-productid={productid}>
-			<img src={images[0]} alt={`the ${name} shoe`} />
-			<div className={styles.body}>
-				<h5 className={styles.name}>{name}</h5>
-				<div>
-					<FaStar />
-					<FaStar />
-					<FaStar />
-					<FaStar />
-					<FaStar />
-				</div>
-				<p className={styles.category}>{category}</p>
-				<p className={styles.price}>£{price}</p>
-				<p className={styles.membership}>member price £{membershipCost}</p>
+  const { name, images, category, price, productid, featured } = props.product;
+  const membershipCost = Math.ceil(price - (price / 100) * 5);
 
-				<Link to={`${props.path}/${productid}`} {...props}>
-					Buy Now
-				</Link>
-			</div>
-		</div>
-	);
+  const sectionPath = props.path.split("/")[1].trim();
+  console.log("Section Path: ", sectionPath);
+
+  return (
+    <div className={styles.shoeCard} data-productid={productid}>
+      <img src={images[0]} alt={`the ${name} shoe`} />
+      <div className={styles.body}>
+        <h5 className={styles.name}>{name}</h5>
+        <div>
+          <FaStar />
+          <FaStar />
+          <FaStar />
+          <FaStar />
+          <FaStar />
+        </div>
+        <p className={styles.category}>{category}</p>
+        <p className={styles.price}>£{price}</p>
+        <p className={styles.membership}>member price £{membershipCost}</p>
+
+        <Link to={`/${sectionPath}/${productid}`} {...props}>
+          Buy Now
+        </Link>
+      </div>
+    </div>
+  );
 };
