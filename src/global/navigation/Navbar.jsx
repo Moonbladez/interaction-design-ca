37276ede8aab2eb668c343e877bb8f9c80d973fb
Frederick M. Rogers <fmrogers@carbonium.no>
2020-05-29 18:17:49 +0200

    Fixed a error that specified <Link /> components need a 'to={}' prop. I did this by turning them into <a /> elements, which removed the error but causes an accesibility issue as the anchors do not have a 'href' attribute.

--- src/global/navigation/Navbar.jsx
+++ src/global/navigation/Navbar.jsx
@@ -8,83 +8,83 @@ import { NavLink, Link } from "react-router-dom";
 import { MegaMenu } from "./MegaMenu";
 
 export class Navbar extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			isOpen: {
-				womensShoesSubnav: false,
-				mensShoesSubnav: false,
-				accessoriesSubnav: false,
-			},
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: {
+        womensShoesSubnav: false,
+        mensShoesSubnav: false,
+        accessoriesSubnav: false,
+      },
+    };
+  }
 
-	handleWomensOpenSubnav = (event) => {
-		event.preventDefault();
-		this.setState({
-			isOpen: {
-				womensShoesSubnav: true,
-				mensShoesSubnav: false,
-				accessoriesSubnav: false,
-			},
-		});
-	};
+  handleWomensOpenSubnav = (event) => {
+    event.preventDefault();
+    this.setState({
+      isOpen: {
+        womensShoesSubnav: true,
+        mensShoesSubnav: false,
+        accessoriesSubnav: false,
+      },
+    });
+  };
 
-	handleMensOpenSubnav = (event) => {
-		event.preventDefault();
-		this.setState({
-			isOpen: {
-				womensShoesSubnav: false,
-				mensShoesSubnav: true,
-				accessoriesSubnav: false,
-			},
-		});
-	};
+  handleMensOpenSubnav = (event) => {
+    event.preventDefault();
+    this.setState({
+      isOpen: {
+        womensShoesSubnav: false,
+        mensShoesSubnav: true,
+        accessoriesSubnav: false,
+      },
+    });
+  };
 
-	handleAccessoriesOpenSubnav = (event) => {
-		event.preventDefault();
-		this.setState({
-			isOpen: {
-				womensShoesSubnav: false,
-				mensShoesSubnav: false,
-				accessoriesSubnav: true,
-			},
-		});
-	};
-	render() {
-		return (
-			<>
-				<nav className={styles.navbar__main}>
-					<NavLink to={"/"}>
-						<h1>Letha</h1>
-					</NavLink>
+  handleAccessoriesOpenSubnav = (event) => {
+    event.preventDefault();
+    this.setState({
+      isOpen: {
+        womensShoesSubnav: false,
+        mensShoesSubnav: false,
+        accessoriesSubnav: true,
+      },
+    });
+  };
+  render() {
+    return (
+      <>
+        <nav className={styles.navbar__main}>
+          <NavLink to={"/"}>
+            <h1>Letha</h1>
+          </NavLink>
 
-					<ul className={styles.main_links}>
-						<li>
-							<Link onClick={this.handleWomensOpenSubnav}>
-								Womens <FaAngleDown />
-							</Link>
-						</li>
-						<li>
-							<Link onClick={this.handleMensOpenSubnav}>
-								Mens Shoes <FaAngleDown />
-							</Link>
-						</li>
-						<li>
-							<Link onClick={this.handleAccessoriesOpenSubnav}>
-								Accessories <FaAngleDown />
-							</Link>
-							<a></a>
-						</li>
-						<li>
-							<NavLink to='/about-us'>About</NavLink>
-						</li>
-					</ul>
+          <ul className={styles.main_links}>
+            <li>
+              <a onClick={this.handleWomensOpenSubnav}>
+                Womens <FaAngleDown />
+              </a>
+            </li>
+            <li>
+              <a onClick={this.handleMensOpenSubnav}>
+                Mens Shoes <FaAngleDown />
+              </a>
+            </li>
+            <li>
+              <a onClick={this.handleAccessoriesOpenSubnav}>
+                Accessories <FaAngleDown />
+              </a>
+              <a></a>
+            </li>
+            <li>
+              <NavLink to="/about-us">About</NavLink>
+            </li>
+          </ul>
 
-					<NavAcc></NavAcc>
-				</nav>
-				<MegaMenu isOpen={this.state.isOpen} />
-			</>
-		);
-	}
+          <NavAcc></NavAcc>
+        </nav>
+        <MegaMenu isOpen={this.state.isOpen} />
+      </>
+    );
+  }
 }
