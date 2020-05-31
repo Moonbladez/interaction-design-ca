d80e5bdac23e2a25d9c917336b6c492d4817c6f0
Bekki Young <moonbladez@hotmail.com>
2020-05-31 12:39:15 +0200

    redesign of product page and megamenu

--- src/components/product-gallery/ProductsGaller.jsx
+++ src/components/product-gallery/ProductsGaller.jsx
@@ -4,54 +4,54 @@ import { v4 as uuid } from "uuid";
 import styles from "./ProductGallery.module.scss";
 
 export class ProductGallery extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      featuredImage: 0,
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			featuredImage: 0,
+		};
+	}
 
-  handleFeaturedImage = ({ target }) => {
-    const imageIndex = target.getAttribute("data-index");
-    this.setState({ featuredImage: imageIndex });
-  };
+	handleFeaturedImage = ({ target }) => {
+		const imageIndex = target.getAttribute("data-index");
+		this.setState({ featuredImage: imageIndex });
+	};
 
-  renderThumbnails = () => {
-    const { images } = this.props;
-    if (images) {
-      return images.map((image, index) => {
-        return (
-          <div className={styles.image} key={uuid()}>
-            <img
-              onClick={this.handleFeaturedImage}
-              src={image}
-              data-index={index}
-            />
-          </div>
-        );
-      });
-    }
-  };
+	renderThumbnails = () => {
+		const { images } = this.props;
+		if (images) {
+			return images.map((image, index) => {
+				return (
+					<div className={styles.image} key={uuid()}>
+						<img
+							onClick={this.handleFeaturedImage}
+							src={image}
+							data-index={index}
+						/>
+					</div>
+				);
+			});
+		}
+	};
 
-  render() {
-    const { images } = this.props;
-    const { featuredImage } = this.state;
-    if (images) {
-      return (
-        <div className={styles.container}>
-          <div className={styles.gallery}>
-            <div className={styles.featured_image}>
-              <img
-                src={featuredImage ? images[featuredImage] : images[0]}
-                data-image-index="0"
-              />
-            </div>
-            <div className={styles.thumbnails}>{this.renderThumbnails()}</div>
-          </div>
-        </div>
-      );
-    } else {
-      return <div>Gallery is loading...</div>;
-    }
-  }
+	render() {
+		const { images } = this.props;
+		const { featuredImage } = this.state;
+		if (images) {
+			return (
+				<div className={styles.container}>
+					<div className={styles.gallery}>
+						<div className={styles.featured_image}>
+							<img
+								src={featuredImage ? images[featuredImage] : images[0]}
+								data-image-index='0'
+							/>
+						</div>
+						<div className={styles.thumbnails}>{this.renderThumbnails()}</div>
+					</div>
+				</div>
+			);
+		} else {
+			return <div>Gallery is loading...</div>;
+		}
+	}
 }
